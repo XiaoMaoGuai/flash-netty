76206838cc92b17cbba2ffc86c9eb11f7dd8def6
weihui <1559018677@qq.com>
2018-09-30 09:44:22 +0800

    push test

--- src/main/java/the/flash/server/NettyServer.java
+++ src/main/java/the/flash/server/NettyServer.java
@@ -16,41 +16,43 @@ import java.util.Date;
 
 public class NettyServer {
 
-    private static final int PORT = 8000;
+	private static final int PORT = 8000;
 
-    public static void main(String[] args) {
-        NioEventLoopGroup boosGroup = new NioEventLoopGroup();
-        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+	public static void main(String[] args) {
+		NioEventLoopGroup boosGroup = new NioEventLoopGroup();
+		NioEventLoopGroup workerGroup = new NioEventLoopGroup();
 
-        final ServerBootstrap serverBootstrap = new ServerBootstrap();
-        serverBootstrap
-                .group(boosGroup, workerGroup)
-                .channel(NioServerSocketChannel.class)
-                .option(ChannelOption.SO_BACKLOG, 1024)
-                .childOption(ChannelOption.SO_KEEPALIVE, true)
-                .childOption(ChannelOption.TCP_NODELAY, true)
-                .childHandler(new ChannelInitializer<NioSocketChannel>() {
-                    protected void initChannel(NioSocketChannel ch) {
+		final ServerBootstrap serverBootstrap = new ServerBootstrap();
+		serverBootstrap
+				.group(boosGroup, workerGroup)
+				.channel(NioServerSocketChannel.class)
+				.option(ChannelOption.SO_BACKLOG, 1024)
+				.childOption(ChannelOption.SO_KEEPALIVE, true)
+				.childOption(ChannelOption.TCP_NODELAY, true)
+				.childHandler(new ChannelInitializer<NioSocketChannel>() {
+
+					@Override
+					protected void initChannel(NioSocketChannel ch) {
 //                        ch.pipeline().addLast(new FirstServerHandler());
-                        ch.pipeline().addLast(new Spliter());
-                        ch.pipeline().addLast(new PacketDecoder());
-                        ch.pipeline().addLast(new LoginRequestHandler());
-                        ch.pipeline().addLast(new MessageRequestHandler());
-                        ch.pipeline().addLast(new PacketEncoder());
-                    }
-                });
+						ch.pipeline().addLast(new Spliter());
+						ch.pipeline().addLast(new PacketDecoder());
+						ch.pipeline().addLast(new LoginRequestHandler());
+						ch.pipeline().addLast(new MessageRequestHandler());
+						ch.pipeline().addLast(new PacketEncoder());
+					}
+				});
 
 
-        bind(serverBootstrap, PORT);
-    }
+		bind(serverBootstrap, PORT);
+	}
 
-    private static void bind(final ServerBootstrap serverBootstrap, final int port) {
-        serverBootstrap.bind(port).addListener(future -> {
-            if (future.isSuccess()) {
-                System.out.println(new Date() + ": 端口[" + port + "]绑定成功!");
-            } else {
-                System.err.println("端口[" + port + "]绑定失败!");
-            }
-        });
-    }
+	private static void bind(final ServerBootstrap serverBootstrap, final int port) {
+		serverBootstrap.bind(port).addListener(future -> {
+			if (future.isSuccess()) {
+				System.out.println(new Date() + ": 端口[" + port + "]绑定成功!");
+			} else {
+				System.err.println("端口[" + port + "]绑定失败!");
+			}
+		});
+	}
 }
